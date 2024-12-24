7fc3502ce37a4e09738f35481771c447dd55991d
Sunil Kumar C <sunil130524@gmail.com>
2024-12-24 18:27:24 +0530

    Use switchMap to get reviews for a given product

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, Observable, of, tap } from 'rxjs';
+import { catchError, map, Observable, of, switchMap, tap } from 'rxjs';
 import { Product } from './product';
+import { ReviewService } from '../reviews/review.service';
+import { Review } from '../reviews/review';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +12,7 @@ export class ProductService {
   private productsUrl = 'api/products';
 
   private http = inject(HttpClient);
+  private reviewService = inject(ReviewService); 
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
@@ -23,9 +26,22 @@ export class ProductService {
     const productUrl = this.productsUrl + '/' + id;
     return this.http.get<Product>(productUrl)
       .pipe(
-        tap(data => console.log(data)),
+        switchMap(product => this.getReviewsByProduct(product)),
         catchError(err => this.handleError(err))
       )
+  }
+
+  getReviewsByProduct(product: Product): Observable<Product> {
+    if(product.hasReviews) {
+      return this.http.get<Review[]>(this.reviewService.getReviewUrl(product.id))
+        .pipe(
+          map((reviews) => {
+            return {...product, reviews} as Product
+          })
+        )
+    } else {
+      return of(product);
+    }
   }
 
   handleError(err: HttpErrorResponse): Observable<never>  {
